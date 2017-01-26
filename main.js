ff4dec562891844fb7604e0212bc219ab0772241
Karla Alves <kalves@kalves-mbr.ca.com>
2017-01-26 11:10:39 -0700

    adding/removing css classes in js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("buton1-btn").addEventListener("click", function(){
+  document.getElementById("message").classList.add("text-style");
+});
+
+document.getElementById("button2-btn").addEventListener("click", function(){
+  document.getElementById("message").classList.remove("text-style");
+});
